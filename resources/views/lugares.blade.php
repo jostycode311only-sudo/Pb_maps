ac0a75baf7b79b4d8984ec2dbc746923b35c287d
Josstin Gómez <jostycode311only@gmail.com>
2025-09-02 17:42:14 -0500

    Agregar carpeta de imagenes y nueva parte de la página.

---
+++ resources/views/lugares.blade.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PBMAPS-lugares turisticos</title>
+    <style>
+        .navegacion {
+            background-color: #333;
+            overflow: hidden;
+        }
+        .navegacion a {
+            float: left;
+            display: block;
+            color: #fff;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        .navegacion a:hover {
+            background-color: #575757;
+        }
+    </style>
+</head>
+<header>
+    <h1>Bienvenido a Lugares Turísticos</h1>
+    <h2>¿Que te gustaría visitar o explorar el día de hoy?</h2>
+    <nav class="navegacion">
+        <a href="#">Lugares Turísticos</a>
+        <a href="#">Eventos</a>
+        <a href="#">Hoteles</a>
+    </nav>
+</header>
+<body>
+    <div class="contenedor_1"></div>
+    <h2>Explorar lugares de interes, que nunca has visitado, cada lugar puede ser una experiencia nueva</h2>
+    <a href="imagenes/cieloroto.jpeg">Cielo Roto</a>
+    
+</body>
+</html>
